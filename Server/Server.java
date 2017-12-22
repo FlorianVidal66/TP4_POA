772880b7c340bd77c29567f41ba70aacf4ff0802
Natacha Guillet <guillet.natacha@outlook.fr>
2017-12-22 16:32:53 -0500

    add arraylsit with the status of sensors

--- Server/Server.java
+++ Server/Server.java
@@ -2,18 +2,24 @@ package Server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 
 public class Server {
 
     public static void main (String[] args) {
         int port = 6666;
+        int nbSensor = 3;
+        ArrayList<String> sensors = new ArrayList<>();
+        for (int i = 0; i < nbSensor; i++) {
+            sensors.add("_");
+        }
         ServerSocket servSock ;
         try {
             servSock = new ServerSocket(port);
             System.out.println("Server.Server started !");
             while(true) {
                 // Hand down the incoming request to a slave
-                Slave slave = new Slave(servSock.accept());
+                Slave slave = new Slave(servSock.accept(), sensors);
                 slave.start();
             }
         } catch (IOException e) {
--- Server/Slave.java
+++ Server/Slave.java
@@ -5,12 +5,15 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Slave extends Thread {
     private Socket sock;
+    private ArrayList<String> sensors;
 
-    public Slave(Socket sock){
+public Slave(Socket sock, ArrayList<String> sensors){
         this.sock = sock;
+        this.sensors = sensors;
     }
 
     @Override
@@ -18,29 +21,28 @@ public class Slave extends Thread {
         try {
             BufferedReader fromBR = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             String command = fromBR.readLine();
-            System.out.println("Command received from Sensor.Sensor"+ command);
+            System.out.println("Command received from Sensor.Sensor    "+ command);
 
             if (command.charAt(0) == 'C') {
                 DataOutputStream toClient = new DataOutputStream(sock.getOutputStream());
                 //TODO : Ici il faut mettre le code pour retourner les infos que le client demande
                 //toClient.writeBytes(result);
-
-
-
             } else {
+                int indexSensor = Integer.parseInt(""+command.charAt(0)) - 1;
                 //TODO : Ici il faut gérer la BDD des capteurs et de leur état en fonction du message reçu
                 //TODO : Ne pas oublier que le message est différent s'il provient du capteur ou d'un filtre (il faut donc dinstinguer les deux cas)
-                /*
-                if (je viens du capteur){
-
-                } else {
-                     Je viens d'un filtre
+                if (command.contains("R")){ //if become to the sensor
+                    sensors.set(indexSensor, ""+command.charAt(command.lastIndexOf("P") + 1));
+                } else { // become to a filter
+                    if (command.substring(command.lastIndexOf(":") + 1).contains("NA"))
+                    {
+                        sensors.set(indexSensor, "0");
+                    } else {
+                        sensors.set(indexSensor, "1");
+                    }
                 }
-                */
             }
-
-
-
+            System.out.println(sensors);
             this.sock.close();
         } catch (IOException e) {
             e.printStackTrace();
